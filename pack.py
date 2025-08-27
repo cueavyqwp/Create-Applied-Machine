2186f1b8dea862724bdc206d40b166906b71a69d
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2025-08-27 19:47:26 +0800

    附加文件

--- pack.py
+++ pack.py
@@ -1,74 +1,63 @@
 
 __version__ = "0.0.1"
 
-if __name__ != "__main__":
-    exit()
+if __name__ == "__main__":
+    import zipfile
+    import shutil
+    import json
+    import os
 
-import requests
+    os.chdir(os.path.dirname(__file__))
 
-import zipfile
-import shutil
-import json
-import os
+    if os.path.isfile("1.21.1-Create.jar"):
+        src = "1.21.1-Create.zip"
+        out = "modrinth.index.json"
 
-os.chdir(os.path.dirname(__file__))
+        files = [
+            {
+                "path": "mods/tacz-neoforge-1.21.1-1.1.6-hotfix-r5.jar",
+                "hashes": {
+                        "sha1": "c4ea60bf4d76716e7e3a7b2f22eb2ab338df51a3",
+                        "sha512": "c28a8a6137c4e89770c57a2251a21adae228609f7b757d18ce3958bc0325900e91d6a4b1eef44eaecab56d24dad120e0639b6dcda2f656d9a7a9df8c61df4e3d"
+                },
+                "downloads": [
+                    "https://github.com/MUKSC/TACZ-1.21.1/releases/download/neoforge-1.1.6-hotfix-r5/tacz-neoforge-1.21.1-1.1.6-hotfix-r5.jar"
+                ],
+                "fileSize": 47612775
+            }
+        ]
 
-if os.path.isfile("1.21.1-Create.jar"):
-    src = "1.21.1-Create.zip"
-    out = "modrinth.index.json"
+        if not os.path.isfile(src):
+            print(f"文件: '{src}' 未找到,请先使用PCL2导出整合包")
 
-    files = [
-        {
-            "path": "mods/tacz-neoforge-1.21.1-1.1.6-hotfix-r5.jar",
-            "hashes": {
-                    "sha1": "c4ea60bf4d76716e7e3a7b2f22eb2ab338df51a3",
-                    "sha512": "c28a8a6137c4e89770c57a2251a21adae228609f7b757d18ce3958bc0325900e91d6a4b1eef44eaecab56d24dad120e0639b6dcda2f656d9a7a9df8c61df4e3d"
-            },
-            "downloads": [
-                "https://github.com/MUKSC/TACZ-1.21.1/releases/download/neoforge-1.1.6-hotfix-r5/tacz-neoforge-1.21.1-1.1.6-hotfix-r5.jar"
-            ],
-            "fileSize": 47612775
-        }
-    ]
+        with zipfile.ZipFile(src, "r") as zf:
+            data = json.loads(zf.read("modrinth.index.json"))
 
-    if not os.path.isfile(src):
-        print(f"文件: '{src}' 未找到,请先使用PCL2导出整合包")
+        data["versionId"] = __version__
+        data["summary"] = "1.21.1-Create"
+        data["files"] += files
 
-    with zipfile.ZipFile(src, "r") as zf:
-        data = json.loads(zf.read("modrinth.index.json"))
+        with open(out, "w", encoding="utf-8") as fp:
+            json.dump(data, fp, ensure_ascii=False, indent=4)
 
-    data["versionId"] = __version__
-    data["summary"] = "1.21.1-Create"
-    data["files"] += files
+        print(f"文件: '{out}' 已更新")
 
-    with open(out, "w", encoding="utf-8") as fp:
-        json.dump(data, fp, ensure_ascii=False, indent=4)
-
-    print(f"文件: '{out}' 已更新")
-
-else:
-    if not os.path.isdir("./src/mods"):
-        os.mkdir("./src/mods")
-
-    if not os.path.isfile("./src/mods/tiabcurio-neoforge-1.21.1-3.0.1.jar"):
-        with open("./src/mods/tiabcurio-neoforge-1.21.1-3.0.1.jar", "wb") as fp:
-            fp.write(requests.get(
-                "https://mediafilez.forgecdn.net/files/6109/580/tiabcurio-neoforge-1.21.1-3.0.1.jar").content)
-
-    os.mkdir("./dist")
-    os.makedirs("./output/overrides")
-    shutil.move("./src/modrinth.index.json", "./output/modrinth.index.json")
-    for path in os.listdir("./src"):
-        if path in (".git", ".github"):
-            continue
-        path = os.path.join("./src", path)
-        shutil.move(path, f"./output/overrides/{os.path.basename(path)}")
-    with zipfile.ZipFile(f"./dist/1.21.1-Create-{__version__}.zip", "w") as zf:
-        os.chdir("output")
-        for root, _, files in os.walk("."):
-            if os.path.split(root)[-1] in (".git", ".github"):
+    else:
+        os.mkdir("./dist")
+        os.makedirs("./output/overrides")
+        shutil.move("./src/modrinth.index.json",
+                    "./output/modrinth.index.json")
+        for path in os.listdir("./src"):
+            if path in (".git", ".github"):
                 continue
-            for file in files:
-                if file in (".gitignore", "export_config.txt", "README.md"):
+            path = os.path.join("./src", path)
+            shutil.move(path, f"./output/overrides/{os.path.basename(path)}")
+        with zipfile.ZipFile(f"./dist/1.21.1-Create-{__version__}.zip", "w") as zf:
+            os.chdir("output")
+            for root, _, files in os.walk("."):
+                if os.path.split(root)[-1] in (".git", ".github"):
                     continue
-                zf.write(os.path.join(root, file))
+                for file in files:
+                    if file in (".gitignore", "export_config.txt", "README.md"):
+                        continue
+                    zf.write(os.path.join(root, file))
---
+++ tacz/README.md
@@ -0,0 +1,15 @@
+# 关于
+
+这里是TaCZ附加枪包所在文件夹
+
+由于modrinth格式整合包限制
+
+只能将附加枪包也包含在整合包内
+
+一下是这些枪包的原链接
+
+[【TACZ】BlueArchive FPS Pack JP](https://www.curseforge.com/minecraft/customization/taczbluearchive-fps-pack-jp) by skylake1008
+[[TaCZ]Blue Archive super gun pack](https://www.curseforge.com/minecraft/customization/tacz-blue-archive-super-gun-pack) by Chuila_Hinata
+[[TaCZ]Applied Armorer](https://www.curseforge.com/minecraft/customization/tacz-applied-armorer-koei) by Koei
+[[TaCZ]Create Armorer](https://www.curseforge.com/minecraft/customization/tacz-create-armorer-koei) by Koei
+[[TaCZ]Immersive Armorer](https://www.curseforge.com/minecraft/customization/tacz-immersive-armorer-koei) by Koei
