71365bc51c77312e81f2749c14c9a26745258d12
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2025-08-28 19:39:41 +0800

    更新至v0.0.2

--- pack.py
+++ pack.py
@@ -1,74 +1,35 @@
 
 __version__ = "0.0.2"
 
+skip = (".git", ".github", ".gitignore",
+        "export_config.txt", "README.md", "cover.py")
+
 if __name__ == "__main__":
     import zipfile
     import shutil
-    import json
     import os
 
-    os.chdir(os.path.dirname(__file__))
+    home = os.path.dirname(__file__)
+    os.chdir(home)
 
     if os.path.isfile("1.21.1-Create.jar"):
-        src = "1.21.1-Create.zip"
-        out = "modrinth.index.json"
+        print("检测在本地,跳过打包")
+        exit()
 
-        files = [
-            {
-                "path": "mods/tacz-neoforge-1.21.1-1.1.6-hotfix-r5.jar",
-                "hashes": {
-                        "sha1": "c4ea60bf4d76716e7e3a7b2f22eb2ab338df51a3",
-                        "sha512": "c28a8a6137c4e89770c57a2251a21adae228609f7b757d18ce3958bc0325900e91d6a4b1eef44eaecab56d24dad120e0639b6dcda2f656d9a7a9df8c61df4e3d"
-                },
-                "downloads": [
-                    "https://github.com/MUKSC/TACZ-1.21.1/releases/download/neoforge-1.1.6-hotfix-r5/tacz-neoforge-1.21.1-1.1.6-hotfix-r5.jar"
-                ],
-                "fileSize": 47612775
-            },
-            {
-                "path": "shaderpacks/BSL_v10.0.zip",
-                "hashes": {
-                        "sha1": "82eb9981a31a4753bb5022b5f64d5ec4f6a27071",
-                        "sha512": "3ce31ae8c7242ae335de70df10f8147420a64ca372585c195d0b300f9e8c5b923f1e5bf2b8dddfcc741a0ce8e0235e079540101632837624787da84e9b52d72f"
-                },
-                "downloads": [
-                    "https://cdn.modrinth.com/data/Q1vvjJYV/versions/jRn8y2VF/BSL_v10.0.zip"
-                ],
-                "fileSize": 1121678
-            }
-        ]
-
-        if not os.path.isfile(src):
-            print(f"文件: '{src}' 未找到,请先使用PCL2导出整合包")
-
-        with zipfile.ZipFile(src, "r") as zf:
-            data = json.loads(zf.read("modrinth.index.json"))
-
-        data["versionId"] = __version__
-        data["summary"] = "1.21.1-Create"
-        data["files"] += files
-
-        with open(out, "w", encoding="utf-8") as fp:
-            json.dump(data, fp, ensure_ascii=False, indent=4)
-
-        print(f"文件: '{out}' 已更新")
-
-    else:
-        os.mkdir("./dist")
-        os.makedirs("./output/overrides")
-        shutil.move("./src/modrinth.index.json",
-                    "./output/modrinth.index.json")
-        for path in os.listdir("./src"):
-            if path in (".git", ".github"):
+    os.mkdir("./dist")
+    os.makedirs("./output/overrides")
+    shutil.move("./src/modrinth.index.json", "./output/modrinth.index.json")
+    shutil.move("./src/CHANGELOG.md", "./output/CHANGELOG.md")
+    shutil.move("./src/LICENSE", "./output/LICENSE")
+    for path in os.listdir("./src"):
+        path = os.path.join("./src", path)
+        shutil.move(path, f"./output/overrides/{os.path.basename(path)}")
+    with zipfile.ZipFile(f"./dist/1.21.1-Create-{__version__}.zip", "w") as zf:
+        os.chdir("./output")
+        for root, _, files in os.walk("."):
+            if os.path.split(root)[-1] in skip:
                 continue
-            path = os.path.join("./src", path)
-            shutil.move(path, f"./output/overrides/{os.path.basename(path)}")
-        with zipfile.ZipFile(f"./dist/1.21.1-Create-{__version__}.zip", "w") as zf:
-            os.chdir("output")
-            for root, _, files in os.walk("."):
-                if os.path.split(root)[-1] in (".git", ".github"):
+            for file in files:
+                if file in skip:
                     continue
-                for file in files:
-                    if file in (".gitignore", "export_config.txt", "README.md", "cover.py"):
-                        continue
-                    zf.write(os.path.join(root, file))
+                zf.write(os.path.join(root, file))
